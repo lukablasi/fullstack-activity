541ecda6aab89b87a0b4ec16ec72de341d5e33bb
unknown <lukablasi@gmail.com>
2021-01-17 10:18:20 +0000

    adding working files

---
+++ app.js
@@ -0,0 +1,119 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+const Product = require('./models/product');
+
+const app = express();
+
+mongoose.connect('mongodb+srv://lukablasi:l79u456k13a@cluster0-pme76.mongodb.net/test?retryWrites=true')
+  .then(() => {
+    console.log('Successfully connected to MongoDB Atlas!');
+  })
+  .catch((error) => {
+    console.log('Unable to connect to MongoDB Atlas!');
+    console.error(error);
+  });
+
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+  next();
+});
+
+app.use(bodyParser.json());
+
+
+
+app.post('/api/products', (req, res, next) => {
+  const product = new Product({
+    name: req.body.name,
+    description: req.body.description,
+    price: req.body.price,
+    inStock: req.body.inStock
+  });
+  product.save().then(
+    (product) => {
+      res.status(201).json({
+        product
+      });
+    }
+  ).catch(
+    (error) => {
+      res.status(400).json({
+        error: error
+      });
+    }
+  );
+});
+
+app.get('/api/products/:id', (req, res, next) => {
+  Product.findOne({
+    _id: req.params.id
+  }).then(
+    (product) => {
+      res.status(200).json(product);
+    }
+  ).catch(
+    (error) => {
+      res.status(404).json({
+        error: error
+      });
+    }
+  );
+});
+
+app.put('/api/products/:id', (req, res, next) => {
+  const product = new Product({
+    _id: req.params.id,
+    name: req.body.name,
+    description: req.body.description,
+    price: req.body.price,
+    inStock: req.body.inStock
+  });
+  Product.updateOne({_id: req.params.id}, product).then(
+    () => {
+      res.status(201).json({
+        message: 'Thing updated successfully!'
+      });
+    }
+  ).catch(
+    (error) => {
+      res.status(400).json({
+        error: error
+      });
+    }
+  );
+});
+
+app.delete('/api/products/:id', (req, res, next) => {
+  Product.deleteOne({_id: req.params.id}).then(
+    () => {
+      res.status(200).json({
+        message: 'Deleted!'
+      });
+    }
+  ).catch(
+    (error) => {
+      res.status(400).json({
+        error: error
+      });
+    }
+  );
+});
+
+app.use('/api/products', (req, res, next) => {
+  Product.find().then(
+    (products) => {
+      res.status(200).json(products);
+    }
+  ).catch(
+    (error) => {
+      res.status(400).json({
+        error: error
+      });
+    }
+  );
+});
+
+module.exports = app;
